b73fdf16d452d15163b6b2998e7a5288e37769e1
zooleen <zooleen@gmail.com>
2020-12-19 18:16:39 +0500

    Finish results.html. Release

--- main.py
+++ main.py
@@ -3,21 +3,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
 @app.route('/dream', methods=['POST'])
 def do_calculate() -> str:
     action = request.form['action']
     hours = int(request.form['hours'])
     minutes = int(request.form['minutes'])
     if action == "asleep":
-        return str(asleep(hours, minutes))
+        answer = str(asleep(hours, minutes))
     if action == "wakeup":
-        return str(wakeup(hours, minutes))
+        answer = str(wakeup(hours, minutes))
+    return render_template('results.html', the_title='Выспись!', the_answer=answer)
 
-@app.route('/entry')
+@app.route('/')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Выспись!')
 
