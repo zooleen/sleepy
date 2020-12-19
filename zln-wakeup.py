52c430b875419bbe90ffd96d4d9e4e8ac03d50aa
zooleen <zooleen@gmail.com>
2020-12-19 13:38:41 +0500

    Add asleep function

--- zln-wakeup.py
+++ zln-wakeup.py
@@ -1,3 +1,15 @@
+def asleep(hours, minutes):
+    ### Returns the wake-up time after specified several sleep cycles.
+    # tim -- Time in Minutes
+    tim = hours * 60 + minutes
+    b = []
+    b.append(normalize(tim + 90 * 3))
+    b.append(normalize(tim + 90 * 4))
+    b.append(normalize(tim + 90 * 5))
+    b.append(normalize(tim + 90 * 6))
+    return(b)
+
+
 def wakeup(hours, minutes):
     ### Returns the wake-up time after specified several sleep cycles.
     # tim -- Time in Minutes
