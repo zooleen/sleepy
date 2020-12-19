22d7f0f613b65a03b7a2d834f2e601c4839d7ce7
zooleen <zooleen@gmail.com>
2020-12-19 13:39:15 +0500

    Add normalize function

--- zln-wakeup.py
+++ zln-wakeup.py
@@ -1,3 +1,9 @@
+def normalize(mins):
+    ### Преобразует входные минуты во время, отбрасывая сутки, если необходимо
+    if mins > 1440:
+        mins -= 1440
+    return(str(mins//60) + ":" + str(mins%60))
+
 def asleep(hours, minutes):
     ### Returns the wake-up time after specified several sleep cycles.
     # tim -- Time in Minutes
