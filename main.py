1bc3c3c34f83d239cf363747786d4dff9feb9286
zooleen <zooleen@gmail.com>
2020-12-19 17:45:01 +0500

    Reorganized directory. Changed entry template

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from dream import normalize, asleep, wakeup
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
+@app.route('/')
+def hello() -> str:
+    return 'Hello world from Flask!'
 
+@app.route('/dream', methods=['POST'])
+def do_calculate() -> str:
+    return str(asleep(7,40))
+
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html', the_title='Выспись!')
+
+app.run(debug=True)
